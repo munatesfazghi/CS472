8a1bd21635e4d3f191d64b4ac540b0f5fe02481f
munatesfazghi <munahaile25@gmail.com>
2023-05-15 19:48:29 -0500

    Exercises updated

--- N2/Node.js
+++ N2/Node.js
@@ -1,11 +1,10 @@
 var http = require('http');
 var dt = require('./myModule');
-http.createServer(function (req, res) {
-res.writeHead(200,
-{'Content-Type': 'text/html'});
-res.write("The date and time arecurrently: ");
-res.end();
-}).listen(8080); 
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write("The date and time are currently: "+dt.myDate());
+    res.end();
+}).listen(8080);
 
 console.log('Server running! ')
-    
