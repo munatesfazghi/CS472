ba893250842d0a34e74b38ceb8108791701a69ee
munatesfazghi <munahaile25@gmail.com>
2023-05-17 09:29:23 -0500

    simple-calculator

--- N3/Node.js
+++ N3/Node.js
@@ -5,4 +5,6 @@ res.writeHead(200, {'Content-Type': 'text/html'});
 var q = url.parse(req.url, true).query;
 var txt = q.year + " " + q.month;
 res.end(txt);
-}).listen(8080); 
+}).listen(8080, ()=>{
+    console.log("server is running")
+}); 
---
+++ Node-Express-Simple Calculator/simple-calculator.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('./public'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/simplecalculator.html');
+});
+app.post('/calculate', (req, res) => {
+    const num1 = parseInt(req.body.num1);
+    const num2 = parseInt(req.body.num2);
+    const operation = req.body.operation;
+    let result;
+    switch (operation) {
+        case ('add'):
+            result = num1 + num2;
+            break;
+        case ('subtract'):
+            result = num1 - num2;
+            break;
+        case ('multiply'):
+            result = num1 * num2;
+            break;
+        case ('divide'):
+            result = num1 / num2;
+        default:
+            result = "Invalid Operation";
+
+    }
+    res.send(`
+    <h1>The Answer is: ${result}</h1>
+    <a href="/">Another calculation</a>
+  `);
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+   
+
+
