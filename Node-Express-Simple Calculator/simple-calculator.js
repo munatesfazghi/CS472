78407f5f284598a381175c1dad1b4b094bfa8bd1
munatesfazghi <munahaile25@gmail.com>
2023-05-19 01:31:27 -0500

    puzzlefifteen and quizlab

--- Node-Express-Simple Calculator/simple-calculator.js
+++ Node-Express-Simple Calculator/simple-calculator.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('./public'));
+app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/simplecalculator.html');
 });
