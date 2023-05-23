0f764542ee3b615ac878157b4adbc3e8c39010c3
munatesfazghi <munahaile25@gmail.com>
2023-05-23 18:04:03 -0500

    updates on apodpromise

--- Js/apodpromise.js
+++ Js/apodpromise.js
@@ -1,23 +1,63 @@
 $(document).ready(
+
     function () {
+
         $("#view_button").click(getPicture);
+
     });
-const params = new URLSearchParams();
-params.append('api_key', 'Ar0slwQxekBVpMi4fFtXmooqdthgEOEvQ94OdREI');
-params.append('date', $("#date").val());
-const url = `https://api.nasa.gov/planetary/apod${params.toString}`;
+
+
+
+
 
 function getPicture() {
+
+    const apiKey = 'Ar0slwQxekBVpMi4fFtXmooqdthgEOEvQ94OdREI';
+
+    const params = new URLSearchParams();
+
+    params.append('api_key', apiKey);
+
+    params.append('date', $("#date").val());
+
+    const url = `https://api.nasa.gov/planetary/apod?${params.toString()}`;
+
     fetch(url)
+
+        .then(response => {
+
+            if (!response.ok) {
+
+                throw new Error('Error: ' + response.status);
+
+            }
+
+            return response.json();
+
+        })
+
         .then(showPicture)
+
         .catch(noPicture)
+
 };
+
+
+
 
 function showPicture(data) {
+
     $("h3").text(data.title);
+
     $("#pic").attr("src", data.url);
 
+
+
+
 };
+
 function noPicture(error) {
+
     alert(error.responseText);
+
 };
